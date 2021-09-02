09cb7b5056507769ca68539f812d4ff16d1a5675
Yectivani <alejandroid36@gmail.com>
2021-09-02 00:37:10 -0500

    quité S.A. al titulo de la pagina

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hospital Guerrero S.A.</title>
+    <title>Hospital Guerrero</title>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 
 
